d66258d77f55ff05460eac7aa8802b8b7df5b2d8
yurafromodessa <yurafromodessa@gmail.com>
2021-11-15 17:00:05 +0200

    repeat

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import someData from './news.json'
+// import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App(){
+    // const data = JSON.parse(someData)
+    // console.log(data)
+    console.log(someData)
+  return(
+      <div className='hi'>
+        {someData.map((item) => (
+        <div key={item.id}>
+            <h1>{item.title}</h1>
+            <h2 dangerouslySetInnerHTML={{__html: item.content}}/>
+            <div>{item.isSpecial ? "Special" : ""}</div>
+            <date>{item.dateCreated.slice(0, 19)}</date>
+            <div>{item.categories.map((cat) => (
+                <div key={cat.id}>
+                    {/*<div>{cat.id}</div>*/}
+                    <p>{cat.name}</p>
+                </div>
+            ))}</div>
+            {item.link && <a href={item.link}>{item.link}</a>}
+            {item.photo && <img src={item.photo + item.id}/>}
+            {item.author && <h4>{item.author}</h4>}
+        </div>
+        ))};
+      </div>
+  )
 }
-
+// function App() {
+//   return (
+//       {someDate.map(some => (
+//           <div key={'id'}>
+//             <title key={'title'}>{'title'}</title>
+//             <div key={'content'}></div>
+//           </div>
+//         ))}
+//   );
+// }
+// function App() {
+//   const reptiles = ['alligator', 'snake', 'lizard'];
+//
+//   return (
+//       <ol>
+//         {reptiles.map(reptile => (
+//             <li key={reptile}>{reptile}</li>
+//         ))}
+//       </ol>
+//   );
+// }
 export default App;
